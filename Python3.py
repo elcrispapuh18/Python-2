8300774093f2190836473a16a14bb2c61230f022
elcrispapuh18 <elcrispapuh123@gmail.com>
2022-08-20 21:23:58 -0500

    Actualizacion

--- Python3.py
+++ Python3.py
@@ -1,12 +1,15 @@
-def divisors(num):
+from ast import Num
+
+
+def divisors(Num):
     divisors=[]
-    for i in range(1,num+1):
-        if num%i==0:
+    for i in range(1,Num+1):
+        if Num%i==0:
             divisors.append(i)
     return divisors
-    
+
     def run ():
-    num=input("Ingrese un número: ")
+    Num=input("Ingrese un número: ")
     assert num.isnumeric(),"Debes ingresar un número"
     print(divisors(int(num)))
     print("Termino mi programa")
